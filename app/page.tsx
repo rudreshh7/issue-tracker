b4219e5fd4867ef6b880a1a7cf56a9c6b482ec7e
Rudresh Jha <kumarrudresh5@gmail.com>
2024-04-29 15:42:58 +0530

    build a navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
-import Navbar from "./Navbar";
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <h1>Hello Work</h1>
     </main>
   );
